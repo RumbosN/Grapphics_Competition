ebf7e5522df683e60a0d5568cbc7dc2b3a670b87
EmilioSello <33448798+Emiliosello@users.noreply.github.com>
2021-06-04 20:07:33 +0200

    UI Básica
    Partes básicas de la UI

---
+++ Graphics_Competition_Unity/Assets/Sigilo/Scripts/UI_Start.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI_Start : MonoBehaviour
+{
+    public GameObject _InfoContainer;
+    private bool _snactivo;
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+    public void StartButtton()
+    {
+        
+        SceneManager.LoadScene("IntroductionPC");
+        //SceneManager.LoadScene("IntroductionVR");
+    }
+
+    public void InfoButton()
+    {
+        if(_snactivo)
+        {
+            _snactivo = false;
+        }
+        else
+        {
+            _snactivo = true;
+        }
+        _InfoContainer.SetActive(_snactivo);
+    }
+
+    public void RestartButton()
+    {
+        SceneManager.LoadScene("StartGame");
+    }
+}
