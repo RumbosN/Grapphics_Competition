526950dd5f2efb473692647ed2d5f0f94a21316e
NorelysR <norelys.rumbos@ludusglobal.com>
2021-06-04 22:36:06 +0200

    anadir jugador a level 1

---
+++ Graphics_Competition_Unity/Assets/HandInteraction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandInteraction : MonoBehaviour
+{
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("ENTER");
+        collision.collider.GetComponent<CheckerCont>()?.ContamidadoCheck();
+    }
+}
