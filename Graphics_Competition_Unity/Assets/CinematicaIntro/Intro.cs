a2dd8a5ea9ec4b630b227c1b55aa8412511919f8
Pepeelipe <Pepeelipe@gmail.com>
2021-06-04 19:42:37 +0200

    Scenes introduction (PC and VR)

---
+++ Graphics_Competition_Unity/Assets/CinematicaIntro/Intro.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour
+{
+    public float timeUntilSceneChange = 0.0f;
+
+    private void Start()
+    {
+        StartCoroutine(ChangeScene());
+    }
+
+    protected IEnumerator ChangeScene() 
+    {
+        yield return new WaitForSeconds(timeUntilSceneChange);
+
+        SceneManager.LoadScene("Level1");
+
+        yield return null;
+    }
+}
