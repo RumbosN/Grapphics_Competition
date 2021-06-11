91b5cf97a6830e0c22724a57f118bad62a8a18c0
Pepeelipe <Pepeelipe@gmail.com>
2021-06-11 19:17:34 +0200

    Particles and Mud

---
+++ Graphics_Competition_Unity/Assets/Sigilo/Scripts/CheckTerrainPlayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckTerrainPlayer : MonoBehaviour
+{
+    public ParticleSystem PS_Mud;
+
+    protected ParticleSystem.EmissionModule _emission;
+
+    private void Start()
+    {
+        if (PS_Mud != null) 
+        {
+            _emission = PS_Mud.emission;
+
+            //Just in case, by default is deactivated
+            _emission.enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Check if player is on Mud
+        if (other.CompareTag("Mud"))
+        {
+            //Sound
+
+            //Particles
+            _emission.enabled = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //Check if player is on Mud
+        if (other.CompareTag("Mud"))
+        {
+            //Sound
+
+            //Particles
+            _emission.enabled = false;
+        }
+    }
+}
