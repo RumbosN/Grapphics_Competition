67883b19e33d87bb92aeeab8e7e71b4a95431e77
EmilioSello <33448798+Emiliosello@users.noreply.github.com>
2021-06-11 17:45:56 +0200

    CreacionClasePuntuacion

---
+++ Graphics_Competition_Unity/Assets/Sigilo/Scripts/Puntuacion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Puntuacion : MonoBehaviour
+{
+    public static int PUNTUACION = 0;
+
+    public static void AddPoints(int value)
+    {
+        PUNTUACION += value;
+    }
+}
