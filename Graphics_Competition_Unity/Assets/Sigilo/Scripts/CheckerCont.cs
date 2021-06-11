515d2e493d1faef3fbe480421b5867c9371c8061
Pepeelipe <Pepeelipe@gmail.com>
2021-06-11 17:49:05 +0200

    Sistema de particulas

--- Graphics_Competition_Unity/Assets/Sigilo/Scripts/CheckerCont.cs
+++ Graphics_Competition_Unity/Assets/Sigilo/Scripts/CheckerCont.cs
@@ -6,9 +6,16 @@ public class CheckerCont : MonoBehaviour
 {
     public Material _materialContaminado;
 
+    public ParticleSystem PS_VirusCompleted;
+
     public void ContamidadoCheck()
     {
         this.GetComponent<Renderer>().sharedMaterial = _materialContaminado;
+
+        //Set particle System
+        var emission = PS_VirusCompleted.emission;
+        emission.enabled = true;
+        
         GameManager.Instance.AddCheck();
     }
 }
